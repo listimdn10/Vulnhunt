6ba16c936973734270f0b1f18a0f2d3a2919e0b2
listimdn10 <quynhnhu170218@gmail.com>
2024-12-10 17:56:49 +0700

    chunk dc r

--- create_database.py
+++ create_database.py
@@ -1,47 +1,38 @@
-from langchain.document_loaders import DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.schema import Document
-from llama_index.core import Document
-
-PDF_FOLDER="./dataset"
-
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core .node_parser import SentenceSplitter 
+from llama_index.core import Settings
 # Hàm tải tài liệu từ thư mục
 def load_documents():
-    loader = DirectoryLoader(PDF_FOLDER, glob="*.md")
-    documents = loader.load()
-    return documents
+    documents = SimpleDirectoryReader("dataset").load_data()
+    print(len(documents))
+    return documents  
 
-# Hàm chia nhỏ văn bản thành chunks
-def split_text(documents):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=4000,  # Kích thước tối đa của mỗi chunk (số ký tự)
-        chunk_overlap=500,  # Độ chồng lấp giữa các chunks
-        length_function=len,
-        add_start_index=True,  # Thêm thông tin về vị trí bắt đầu của chunk
+
+# Hàm chia nhỏ văn bản thành chunks,  this class tries to keep sentences and paragraphs together
+def split_doc_to_chunk(documents):
+    # Khởi tạo TextSplitter
+    text_splitter = SentenceSplitter(
+        chunk_size=300,  # Kích thước tối đa mỗi chunk
+        chunk_overlap=100,  # Độ chồng lấp giữa các chunk
     )
-    chunks = text_splitter.split_documents(documents)
-    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+    # Split văn bản thành các chunk
+    chunks = []
+    for doc in documents:
+        # print(f"Document length: {len(doc.text)}")
 
+        # Split từng document thành các chunk
+        split_chunks = text_splitter.split_text(doc.text)
+        chunks.extend(split_chunks)
+    
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+    
     # In ví dụ về một chunk
-    document = chunks[0]
-    print(document.page_content)
-    print(document.metadata)
+    # print(chunks[2]) # In ra chunk đầu tiên
     return chunks
 
 
-# def chunks_to_documents(chunks):
-#     documents = []
-#     for chunk in chunks:
-#         documents.append(Document(
-#             text="chunk.page_content",  # Nội dung văn bản
-#             doc_id=chunk.metadata.get( 'start_index'),  # ID duy nhất
-#             extra_info={'chunk_metadata': chunk.metadata}  # Metadata bổ sung
-#         ))
-#     print(len(documents))
-#     documents[0]
-#     return documents
 
 if __name__ == "__main__":
     documents= load_documents()
-    chunks = split_text(documents)
-    llamaindex_document = chunks_to_documents(chunks)
+    chunks = split_doc_to_chunk(documents)
+ 
