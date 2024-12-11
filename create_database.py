2539dd7e1a388f7a53173e95242b692849fe3c0f
listimdn10 <quynhnhu170218@gmail.com>
2024-12-11 18:14:13 +0700

    them pinecone

--- create_database.py
+++ create_database.py
@@ -1,6 +1,16 @@
-from llama_index.core import SimpleDirectoryReader
+from llama_index.core import SimpleDirectoryReader, ServiceContext, GPTVectorStoreIndex
 from llama_index.core .node_parser import SentenceSplitter 
 from llama_index.core import Settings
+import openai 
+from pinecone import Pinecone, ServerlessSpec
+import os
+
+from llama_index.vector_stores.pinecone import PineconeVectorStore
+from llama_index.core import GPTVectorStoreIndex, StorageContext, Settings
+from llama_index.embeddings.openai import OpenAIEmbedding
+
+
+
 # Hàm tải tài liệu từ thư mục
 def load_documents():
     documents = SimpleDirectoryReader("dataset").load_data()
@@ -19,7 +29,6 @@ def split_doc_to_chunk(documents):
     chunks = []
     for doc in documents:
         # print(f"Document length: {len(doc.text)}")
-
         # Split từng document thành các chunk
         split_chunks = text_splitter.split_text(doc.text)
         chunks.extend(split_chunks)
@@ -31,8 +40,49 @@ def split_doc_to_chunk(documents):
     return chunks
 
 
+#tao index tren Pinecone
+def upload_to_pinecone(): 
+    # find API key in console at app.pinecone.io
+    pc = Pinecone(api_key="pcsk_3g4mTY_HJ4TSsa17ZFrUDXxssVSKLxnVRZGvMiqUp84PrkrqeWF7YXhvbuADN5B4vnu1fz")
+    # create the index if it does not exist already
+    index_name = "vulnhunt-gpt"
+
+    pc.create_index(
+        name=index_name,
+        dimension=1536, # Replace with your model dimensions
+        metric="cosine", # Replace with your model metric
+        spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+        ) 
+    )
+        # connect to the index
+    pinecone_index = pc.Index(index_name)
+    
+    #create 
+    # we can select a namespace (acts as a partition in an index)
+    namespace = '' # default namespace
+    vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
+    # setup our storage (vector db)
+    storage_context = StorageContext.from_defaults(
+        vector_store=vector_store
+    )
+
+    import os
+    # https://platform.openai.com/account/api-keys
+    os.environ["OPENAI_API_KEY"] = "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"
+
+
+    # setup the index/query process, ie the embedding model (and completion if used)
+    embed_model = OpenAIEmbedding(model='text-embedding-ada-002', embed_batch_size=100)
+    service_context = ServiceContext.from_defaults(embed_model=embed_model)
+
+    index = GPTVectorStoreIndex.from_documents(
+        documents, storage_context=storage_context,
+        service_context=service_context
+    )
 
 if __name__ == "__main__":
     documents= load_documents()
     chunks = split_doc_to_chunk(documents)
- 
+    upload_to_pinecone()
